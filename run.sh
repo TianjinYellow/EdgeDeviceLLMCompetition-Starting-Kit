0f827a34ed67c2aff5b2bc1506ac3430edd825e8
TienjinHuang <t.huang@tue.nl>
2024-06-23 11:29:49 +0200

    Update README-9

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python evaluate.py --datasets ceval_ppl mmlu_ppl --hf-type base --hf-path microsoft/phi-2
+cd opencompass
+CUDA_VISIBLE_DEVICES=0 python run.py --datasets humaneval_gen --hf-num-gpus 1 --hf-type base --models example --debug --model-kwargs device_map='auto' trust_remote_code=True
+
+#tested-model
+#microsoft/phi-2
+
+#tested-datasets
+#commonseqa_gen+longbench bbh_gen gsm8k_gen humaneval_gen FewCLUE_chid_gen truthfulqa_gen
+
+#/scratch-shared/HTJ/llama_prune/pytorch_model.bin
