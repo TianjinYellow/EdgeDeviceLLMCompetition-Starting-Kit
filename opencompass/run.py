2fa9eab016983f57d9b28050af515a42e5faa483
TienjinHuang <t.huang@tue.nl>
2024-07-20 07:52:37 +0100

    Update run.py

--- opencompass/run.py
+++ opencompass/run.py
@@ -3,11 +3,11 @@ os.environ["MKL_SERVICE_FORCE_INTEL"] = '1'
 os.environ["MKL_THREADING_LAYER"] = '1'
 
 #os.environ['TRANSFORMERS_CACHE'] = '/scratch-share/transformers'
-os.environ['HF_DATASETS_CACHE'] = '/scratch-shared/'
-os.environ['HF_TOKENIZERS_CACHE'] = '/scratch-shared/tokenizes'
-os.environ['HF_HOME'] = '/scratch-shared/HF_HOME'
-os.environ['HF_METRICS_CACHE'] = '/scratch-shared/metrics'
-os.environ['HF_MODULES_CACHE'] = '/scratch-shared/modules'
+#os.environ['HF_DATASETS_CACHE'] = '/scratch-shared/'
+#os.environ['HF_TOKENIZERS_CACHE'] = '/scratch-shared/tokenizes'
+#os.environ['HF_HOME'] = '/scratch-shared/HF_HOME'
+#os.environ['HF_METRICS_CACHE'] = '/scratch-shared/metrics'
+#os.environ['HF_MODULES_CACHE'] = '/scratch-shared/modules'
 
 from opencompass.cli.main import main
 
